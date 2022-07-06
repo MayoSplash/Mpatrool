4ae9ef815015362eadbc36a41c7b7a1e4a1df54f
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-06 23:23:25 +0300

    Работает wall.get

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0
+import json
+
 from flask import Flask
 from flask_restful import Api, Resource
 import requests
@@ -7,15 +9,16 @@ import vk_api
 app = Flask(__name__)
 api = Api()
 
-session = vk_api.VkApi(login="89883451024", password="Cucumber_2")
-session.auth()
+#session = vk_api.VkApi(login="89883451024", password="Cucumber_2")
+session = vk_api.VkApi(token="9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0")
 vk = session.get_api()
 
 class Main(Resource):
-    def get(self, user_id):
-        return vk.account.getProfileInfo(user_id=192715293)
+    def get(self, user_ids):
+        res = vk.wall.get(owner_id=user_ids)
+        return res
 
-api.add_resource(Main, "/<int:user_id>")
+api.add_resource(Main, "/<int:user_ids>")
 api.init_app(app)
 
 if __name__ == "__main__":
