d37b816001d1437b1a557c6cd3758fe05d22b646
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-06 23:41:29 +0300

    рефакторинг

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-#9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0
-import json
-
 from flask import Flask
 from flask_restful import Api, Resource
-import requests
 import vk_api
 
 app = Flask(__name__)
 api = Api()
 
-#session = vk_api.VkApi(login="89883451024", password="Cucumber_2")
-session = vk_api.VkApi(token="9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0")
+accsess_token = "9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0"
+session = vk_api.VkApi(token=accsess_token)
 vk = session.get_api()
 
 class Main(Resource):
