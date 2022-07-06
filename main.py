1dde488d866b05557dbcddb73c0c7cb264eb32a1
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-06 22:45:11 +0300

    Что-то пошло не так, пересобрал все

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
+#9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0
 from flask import Flask
 from flask_restful import Api, Resource
+import requests
+import vk_api
 
 app = Flask(__name__)
 api = Api()
 
+session = vk_api.VkApi(login="89883451024", password="Cucumber_2")
+session.auth()
+vk = session.get_api()
+
 class Main(Resource):
     def get(self, user_id):
-        return {"1": user_id}
-
-
-
-
-
-
-
+        return vk.account.getProfileInfo(user_id=192715293)
 
 api.add_resource(Main, "/<int:user_id>")
 api.init_app(app)
