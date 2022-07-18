d6618e8477ad333807912efab7ab303cd009e23a
MayoSplash <roman.stepanenko.01@gmail.com>
2022-07-18 13:20:03 +0300

    save

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from flask_restful import Api, Resource
 import vk_api
 
 app = Flask(__name__)
-api = Api()
+api = Api(prefix='/api')
 
 accsess_token = "9845b98b9845b98b9845b98b6e983ec12d998459845b98bfa5ac65ed421762106e3abf0"
 session = vk_api.VkApi(token=accsess_token)
 vk = session.get_api()
 
-class Main(Resource):
-    def get(self, user_ids):
-        res = vk.wall.get(owner_id=user_ids)
+class Wall(Resource):
+    def get(self, id):
+        res = vk.wall.get(owner_id=id)
         return res
 
-api.add_resource(Main, "/<int:user_ids>")
+class Account(Resource):
+    def get(self, id):
+        res = vk.users.get(user_id=id)
+        return res
+
+
+
+api.add_resource(Wall, "/wall/<int:id>")
+api.add_resource(Account, "/user/<int:id>")
 api.init_app(app)
 
 if __name__ == "__main__":
